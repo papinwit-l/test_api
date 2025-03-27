45843ec35961d6df5e9cb2be0d14148d75bfc079
papinwit-l <pae.papinwit@gmail.com>
2025-03-27 17:43:51 +0700

    first

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const port = 8000;
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", route);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+const route = async (req, res, next) => {
+  console.log(req.body);
+  res.send("hello world");
+};
