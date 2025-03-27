3b34d07c83f431f26c28177d6c0cf2ec9882b8d6
papinwit-l <pae.papinwit@gmail.com>
2025-03-27 17:55:15 +0700

    fix2

--- src/index.js
+++ src/index.js
@@ -8,8 +8,12 @@ app.use(cors());
 app.use(express.json());
 
 const route = async (req, res, next) => {
-  console.log(req.body);
-  res.send("hello world");
+  try {
+    console.log(req.body);
+    res.status(200).json({ message: "hello world" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 app.use("/api", route);
